9ac583b9e36fca8083a6c8653419fd267f402b4b
S.Iman <simangali2002@gmail.com>
2023-03-18 21:09:59 +0600

    style: added pre git hook lints

---
+++ branchnamelint.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    pattern: ':type/:name',
+    params: {
+      type: [
+        'fix',
+        'hotfix',
+        'feat',
+        'feature',
+        'docs',
+        'misc',
+        'improve',
+        'introduce',
+      ],
+      name: ['[a-z0-9-]+'],
+    },
+    prohibited: [
+      'ci',
+      'wip',
+      'main',
+      'dev',
+      'develop',
+      'production',
+      'test',
+      'build',
+      'master',
+      'release',
+    ],
+  };

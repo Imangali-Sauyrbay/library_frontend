844b781c592d52203e25fd45293ddea2fa8e36d8
S.Iman <simangali2002@gmail.com>
2023-03-18 21:11:12 +0600

    fix(style): added env node rule for config files

--- branchnamelint.config.js
+++ branchnamelint.config.js
@@ -1,28 +1,29 @@
+/* eslint-env node */
 module.exports = {
-    pattern: ':type/:name',
-    params: {
-      type: [
-        'fix',
-        'hotfix',
-        'feat',
-        'feature',
-        'docs',
-        'misc',
-        'improve',
-        'introduce',
-      ],
-      name: ['[a-z0-9-]+'],
-    },
-    prohibited: [
-      'ci',
-      'wip',
-      'main',
-      'dev',
-      'develop',
-      'production',
-      'test',
-      'build',
-      'master',
-      'release',
+  pattern: ':type/:name',
+  params: {
+    type: [
+      'fix',
+      'hotfix',
+      'feat',
+      'feature',
+      'docs',
+      'misc',
+      'improve',
+      'introduce',
     ],
-  };
+    name: ['[a-z0-9-]+'],
+  },
+  prohibited: [
+    'ci',
+    'wip',
+    'main',
+    'dev',
+    'develop',
+    'production',
+    'test',
+    'build',
+    'master',
+    'release',
+  ],
+ };
